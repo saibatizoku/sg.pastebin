913854ca5425f08210a4ddfaace5bd790a019103
Joaquin R <globojorro@gmail.com>
2011-04-19 18:33:52 -0500

    Initial commit based on ZopeSkel's 'plone' template

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='sg.pastebin',
+      version=version,
+      description="Allows for snippets of source code to be added as a content type for Plone 4>",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='plone dexterity pastebin',
+      author='Joaquín Rosales',
+      author_email='globojorro@gmail.com',
+      url='https://github.com/saibatizoku/sg.pastebin',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['sg'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
