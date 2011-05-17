d2dcb38eff2ff0e040b18e368aab8b89e6a30c64
Joaquín Rosales <globojorro@gmail.com>
2011-05-17 16:52:31 -0500

    Added control panel configlet

---
+++ sg/pastebin/controlpanel.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+from five import grok
+
+from zope.interface import Interface
+from zope import schema
+from zope.component import getMultiAdapter
+from Products.CMFCore.utils import getToolByName
+
+from Products.CMFPlone.interfaces import IPloneSiteRoot
+from plone.app.registry.browser import controlpanel
+
+from sg.pastebin import _
+
+
+class ISGPastebinSettings(Interface):
+    """ Interface for the form on the control panel. """
+    preferred_languages = schema.Set(title=_(u"Preferred coding languages"),
+            description=_(u"A list of coding languages that will be highlighted according to their specific syntax."),
+            required=False,
+            default=set([u"ActionScript",
+                        u"Bash",
+                        u"C",
+                        u"C#",
+                        u"C++",
+                        u"CSS",
+                        u"HTML",
+                        u"Java",
+                        u"JavaScript",
+                        u"Lua",
+                        u"Perl",
+                        u"PHP",
+                        u"Python",
+                        u"SQL",
+                        u"Ruby"]),
+            value_type= schema.TextLine(title=_(u"Language")))
+
+    preferred_styles = schema.Set(title=_(u"Styles for highlighting syntax"),
+            required=False,
+            default=set([]),
+            value_type = schema.TextLine(title=_(u"Highlighting style"))
+            )
+
+class SGPastebinSettingsEditForm(controlpanel.RegistryEditForm):
+    grok.context(IPloneSiteRoot)
+    grok.name("sg_questions_settings")
+    grok.require("cmf.ManagePortal")
+
+    schema = ISGPastebinSettings
+    label = _(u"SoftwareGuru Pastebin Settings") 
+    description = _(u"Here you can modify the settings for SG Pastebin.")
+
+    def updateFields(self):
+        super(SGPastebinSettingsEditForm, self).updateFields()
+
+    def updateWidgets(self):
+        super(SGPastebinSettingsEditForm, self).updateWidgets()
+        self.widgets['preferred_languages'].rows = 15
+        self.widgets['preferred_languages'].style = u'width: 30%;'
+        self.widgets['preferred_styles'].rows = 10
+        self.widgets['preferred_styles'].style = u'width: 30%;'
+
+class SGPastebinSettingsControlPanel(controlpanel.ControlPanelFormWrapper):
+    form = SGPastebinSettingsEditForm
--- sg/pastebin/paste.py
+++ sg/pastebin/paste.py
@@ -3,10 +3,15 @@ from five import grok
 from zope import schema
 from plone.namedfile.field import NamedFile
 
-from zope.schema.interfaces import IContextSourceBinder
+from zope.component import getUtility
+
+from zope.schema.interfaces import IVocabularyFactory
 from zope.schema.vocabulary import SimpleTerm
 from zope.schema.vocabulary import SimpleVocabulary
+
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+
+from plone.memoize.instance import memoize
 
 from plone.directives import form, dexterity
 from plone.app.textfield import RichText
@@ -16,25 +21,29 @@ from pygments.lexers import get_all_lexers
 from pygments.lexers import get_lexer_by_name
 from pygments.formatters import HtmlFormatter
 
+from plone.registry.interfaces import IRegistry
+from sg.pastebin.controlpanel import ISGPastebinSettings
 from sg.pastebin import _
 
 class ISGPastebinLayer(IDefaultBrowserLayer):
     """ Browser layer for sg.pastebin """
 
-"""
-@grok.provider(IContextSourceBinder)
-def lexer_vocabulary(context=None):
-    cnt = 0
-    terms = []
-    for lexer in get_all_lexers():
-        cnt += 1
-        terms.append(SimpleTerm(token=cnt, value=lexer[0], title=lexer[0]))
-    return None # SimpleVocabulary(terms)
-"""
+class LanguageVocabulary(object):
+    grok.implements(IVocabularyFactory)
+
+    def __call__(self, context):
+        registry = getUtility(IRegistry)
+        settings = registry.forInterface(ISGPastebinSettings)
+        terms = []
+        for lang in sorted(settings.preferred_languages):
+            terms.append(SimpleTerm(value=str(lang).lower(), title=lang))
+        return SimpleVocabulary(terms)
+
+grok.global_utility(LanguageVocabulary, name=u"sg.pastebin.languages")
 
 class IPaste(form.Schema):
     """ A content-type for source code snippets. """
-    lexer  = schema.Choice(title=_(u"Language"), values=['python', 'php'], default='python')
+    lexer  = schema.Choice(title=_(u"Language"), vocabulary=u"sg.pastebin.languages", default='python')
     code = schema.SourceText(title=_(u"Source code"))
     codefile = NamedFile(title=_(u"Source-code file"), required=False)
 
@@ -44,6 +53,8 @@ class View(grok.View):
     grok.require('zope2.View')
     grok.layer(ISGPastebinLayer)
 
+
+    @memoize
     def pygmented(self):
         """ Returns highlighted source code """
         lx = get_lexer_by_name(self.context.lexer)
